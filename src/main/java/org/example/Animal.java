2f070ab15991da3806adbe3387557e0f7bf5bda2
lageniaestela <estelam.escobar@gmail.com>
2023-06-12 16:33:40 -0300

    Initial commit

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public  abstract class Animal {
+    private String nombre;
+    public Animal(){
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void comer(){
+        System.out.println(this.getNombre()+"esta comiendo");
+    }
+    public void caminar(){
+        System.out.println(this.getNombre()+"esta caminando");
+    }
+}
